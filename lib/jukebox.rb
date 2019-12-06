c721bfd0c8306c9b5c53033b0710803055198dc9
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-06 21:38:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,11 +26,11 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  response = gets.chomp
-  if response.to_i >= 1 && response.to_i <= songs.length
-    puts "Playing #{songs[response.to_i-1]}"
-  elsif songs.include?(response)
-    puts "Playing #{songs.find{|song| song == response}}"
+  answer = gets.chomp
+  if answer.to_i >= 1 && answer.to_i <= songs.length
+    puts "Playing #{songs[answer.to_i-1]}"
+  elsif songs.include?(answer)
+    puts "Playing #{songs.find{|song| song == answer}}"
   else
     puts "Invalid input, please try again"
   end
